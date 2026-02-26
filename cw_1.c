962b1384f49f543f07db5f3163871ea92d264a1b
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2026-02-26 18:08:24 +0100

    Comment out multiple printf and scanf statements

--- cw_1.c
+++ cw_1.c
@@ -1,1 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+int main()
+{
+//1.2.1
+//printf("Hello world!\n");
+
+//1.2.2
+//printf("Bardzo \ndlugi \nnapis\n");
+
+//1.2.3
+//printf("Napis zawierajacy rozne dziwne znaczki // \\ \\ $ & %%");
+
+//1.2.4
+//int l;
+//printf("Podaj liczbe calkowita: ");
+//scanf("%d", &l);
+//printf("Ta liczba to: %d", l);
+
+//1.2.5
+//double l;
+//printf("Podaj liczbe calkowita: ");
+//scanf("%lf", &l);
+//printf("Ta liczba to: %lf", l);
+
+//1.2.6
+//int l1, l2, l3;
+//printf("Podaj 3 liczby calkowite oddzielone spacja: ");
+//scanf("%d %d %d", &l1, &l2, &l3);
+//printf("%d \n%d \n%d", l1, l2, l3);
+
+//1.2.7
+//int l;
+//printf("Podaj liczbe calkowita: ");
+//scanf("%d", &l);
+//printf("Ta liczba to: %d", l+1);
+
+//1.2.8
+//int l1, l2, l3;
+//printf("Podaj 3 liczby calkowite oddzielone spacja: ");
+//scanf("%d %d %d", &l1, &l2, &l3);
+//double s = (l1+l2+l3)/3.0;
+//printf("\n%lf", s);
+
+//1.2.9
+//double x;
+//printf("Podaj x: ");
+//scanf("%lf", &x);
+//printf("Wynik: %lf\n", sqrt(x));
+
+//1.2.10
+//double x;
+//printf("Podaj liczbe wymierna: ");
+//scanf("%lf", &x);
+//double x2 = (x < 0) ? -x : x;
+//printf("%lf", x2);
+
+//1.2.11
+
+
+//1.2.12
+
+
+
+
+    return 0;
+}
