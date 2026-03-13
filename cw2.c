20a4304cb49451c02edcd88df21627fd21a63f8f
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2026-03-13 11:55:11 +0100

    Update cw2.c

--- cw2.c
+++ cw2.c
@@ -2,22 +2,136 @@
 #include <stdlib.h>
 #include <math.h>
 
+void bez(int a)
+{
+    if (a<0)
+    {
+        a=a*(-1);
+    }
+    printf("Wynik to: %d\n",a);
+}
+
+void silnia(int a)
+{
+    int b=a-1;
+    while (b>0)
+    {
+        a=a*b;
+        b=b-1;
+    }
+    printf("Wynik to: %d\n",a);
+}
+
+void pom(int n)
+{
+    int k=n-1;
+    while (k>0)
+    {
+        if (n%k==0)
+        {
+        printf("Wynik to: %d\n",k);
+        return;
+        }
+        else
+        {
+        k=k-1;
+        }
+    }
+}
+
+void potega(int n)
+{
+    int a =n;
+    int w =2;
+    if (a==1)
+    {
+    printf("Wynik to: 2");
+    }
+    else
+    {
+    n=n-1;
+    while(n>0)
+    {
+        w = w*2;
+        n=n-1;
+    }
+    printf("Wynik to: %d\n",w);
+    }
+}
+
+void potega2(int n)
+{
+    int a =n;
+    int w =2;
+    if (a==1)
+    {
+    printf("Wynik to: 2");
+    }
+    else if (a==0)
+    {
+    printf("Wynik to: 0");
+    }
+    else if (a==-1)
+    {
+    printf("Wynik to: -2");
+    }
+    else
+    {
+    if (a>0)
+    {
+        n=n-1;
+    while(n>0)
+    {
+        w = w*2;
+        n=n-1;
+    }
+    printf("Wynik to: %d\n",w);
+    }
+    else
+    {
+    n=n*(-1);
+    n=n-1;
+    while(n>0)
+    {
+        w = w*2;
+        n=n-1;
+    }
+    printf("Wynik to: %d\n",w);
+    }
+    }
+}
+
 int main()
 {
 //2.2.1
-int bez()
+//int n;
+//printf("Podaj liczbe calkowita: ");
+//scanf("%d", &n);
+//bez(n);
 
 //2.2.2
-
+//int n;
+//printf("Podaj liczbe calkowita: ");
+//scanf("%d", &n);
+//silnia(n);
 
 //2.2.3
-
+//int n;
+//printf("Podaj liczbe calkowita n>2: ");
+//scanf("%d", &n);
+//pom(n);
 
 //2.2.4
-
+//int n;
+//printf("Podaj liczbe calkowita: ");
+//scanf("%d", &n);
+//potega(n);
 
 //2.2.5
-
+int n;
+printf("Podaj liczbe calkowita: ");
+scanf("%d", &n);
+potega2(n);
 
 //2.2.6
 
